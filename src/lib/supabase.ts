02e3fa8f53ae30f9afd244b91bdf64044d8d942a
Stephen Christie <stephen228christie@gmail.com>
2026-06-10 20:19:37 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://vkytcuefcddnynesthlq.supabase.co";
+const SUPABASE_URL = "https://zxanxrgjfrfywwzgsrwy.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZreXRjdWVmY2RkbnluZXN0aGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MDI5NzEsImV4cCI6MjA5NTk3ODk3MX0.yiirwF57nVW93GcoZCzgu4wujueTOWcdqF6B_BcOVw4";
+  "sb_publishable_MMwrkcT_rGOVhCcaV61A9g_3eEFnKZ8";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
